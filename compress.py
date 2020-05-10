394fdd1a68e20fd4d0ef1692809cd460bf2889ff
yash <yashsuvarna2001@gmail.com>
2020-05-10 06:59:18 +0530

    The final version no GUI made

--- compress.py
+++ compress.py
@@ -1,24 +1,25 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 import os
 import sys
+
+
 from PIL import Image
 import numpy as np
 
-def open_image(path):                                                 #Open image and get the values for RGB for each pixel                                           
-    image=Image.open(path)    
-    
+def open_file(path):                                                     #Open image and get the values for RGB for each pixel                                           
+    image=Image.open(path)  
     return np.asarray(image)/255
-
+          
 def K_random_centroid(A , K):                                         #Randomly choose K points in the image array
     m=len(A)                                                   
     return A[np.random.choice(m,K,replace=False),:]
 
 def closest_centroid(A,centroids):                                    #Find the centroid closest to any given image point by calculating the norm of RGB
     m=len(A)
-    c=np.zeros(m)                            
+    c=np.zeros(m)
     for i in range(m):
-        distances = np.linalg.norm(A[i] - centroids, axis=1)
+        distances = np.linalg.norm(A[i] - centroids, axis=1)          #For each pixel determine the 'distance' from every centroid and the choose the least as it's closest centroid
         c[i] = np.argmin(distances)            
     return c
 
@@ -28,28 +29,26 @@ def compute_means(A, idx, K):                                         #For any c
     centroids = np.zeros((K, n))
     for k in range(K):
         examples = A[np.where(idx == k)]
-        mean = [np.mean(column) for column in examples.T]
+        mean = [np.mean(column) for column in examples.T]             #For every centroid find the mean of R,G,B values of each point for which it is the closest centroid
         centroids[k] = mean
     return centroids
 
-
-
-
-def find_K(A,K,max_iters=20):                                         #Tune the centroid to get the best set of K points that are best suited for the image
+def find_K(A,K,max_iters=10):                                         #Tune the centroid to get the best set of K points that are best suited for the image
     centroids=K_random_centroid(A,K)
     prev_centroids=centroids
-    for _ in range(max_iters):
+    for j in range(max_iters):
         idx=closest_centroid(A,centroids)               
         centroids = compute_means(A, idx, K)       
-        if ( prev_centroids==centroids).all():
-            return centroids                            
+        if ( prev_centroids==centroids).all():                        #If the centroids dont move we get the optimal centroid for each and every point
+            return centroids
         else:
             prev_centroids=centroids
     return centroids,idx
         
     
 def main():
-    image=open_image('imeg.jpeg')                                      
+    
+    image=open_file("image.jpeg")                                      #Input
         
     w,h,d=image.shape                                  
         
@@ -58,14 +57,14 @@ def main():
     K=int(input("Enter the scale value : "))
     print("Procedure begins ....")
     colors,_=find_K(A,K,max_iters=10)                 
-    ch=int(input("Enter the output type"))
+    ch=int(input("Enter the output type "))
     idx=closest_centroid(A ,colors)      
-    #Reconstruct the image           
+                                                                     #Reconstruct the image           
     idx = np.array(idx, dtype=np.uint8)
-    A_reconstructed = np.array(colors[idx, :] * ch, dtype=np.uint8).reshape((w, h, d))
-    #Save the output file 
+    A_reconstructed = np.array(colors[idx, :] * ch, dtype=np.uint8).reshape((w,h,d))
+                                                                     #Save the output file 
     compressed_image = Image.fromarray(A_reconstructed)
-    compressed_image.save('out.jpeg')
+    compressed_image.save('out2.jpeg')
                
 if __name__ == '__main__':
     main()
